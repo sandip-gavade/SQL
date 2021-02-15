3b71b261d32aea42a8f5899b5c6353dc43bc4f01
sandip-gavade <gavadesandip81@gmail.com>
2021-02-15 21:52:00 +0530

    view and index code sql added

---
+++ view and index.sql
@@ -0,0 +1,92 @@
+
+CREATE TABLE CUSTOMERS(
+   ID   INT              NOT NULL,
+   NAME VARCHAR (20)     NOT NULL,
+   AGE  INT              NOT NULL,
+   ADDRESS  CHAR (25) ,
+   SALARY   DECIMAL (18, 2),       
+   PRIMARY KEY (ID)
+);
+
+INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (1, 'Ramesh', 32, 'Ahmedabad', 2000.00 );
+
+INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (2, 'Khilan', 25, 'Delhi', 1500.00 );
+
+INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (3, 'kaushik', 23, 'Kota', 2000.00 );
+
+INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (4, 'Chaitali', 25, 'Mumbai', 6500.00 );
+
+INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (5, 'Hardik', 27, 'Bhopal', 8500.00 );
+
+INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY)
+VALUES (6, 'Komal', 22, 'MP', 4500.00 );
+
+CREATE TABLE ORDERS (
+   ID          INT        NOT NULL,
+   DATE        DATETIME, 
+   CUSTOMER_ID INT references CUSTOMERS(ID),
+   AMOUNT     double,
+   PRIMARY KEY (ID)
+);
+
+
+ALTER TABLE ORDERS 
+   ADD FOREIGN KEY (Customer_ID) REFERENCES CUSTOMERS (ID);
+   
+
+INSERT INTO `practice`.`orders` (`ID`, `DATE`, `CUSTOMER_ID`, `AMOUNT`) VALUES
+ ('102', '2009-10-08 00:00:00', '1', '500');
+ 
+ INSERT INTO `practice`.`orders` (`ID`, `DATE`, `CUSTOMER_ID`, `AMOUNT`) VALUES
+ ('100', '2009-10-03 00:00:00', '2', '1500');
+ 
+ INSERT INTO `practice`.`orders` (`ID`, `DATE`, `CUSTOMER_ID`, `AMOUNT`) VALUES
+ ('101', '2009-10-04 00:00:00', '3', '2500');
+ 
+ INSERT INTO `practice`.`orders` (`ID`, `DATE`, `CUSTOMER_ID`, `AMOUNT`) VALUES
+ ('103', '2009-10-05 00:00:00', '3', '3500');
+ 
+ INSERT INTO `practice`.`orders` (`ID`, `DATE`, `CUSTOMER_ID`, `AMOUNT`) VALUES
+ ('104', '2009-10-07 00:00:00', '4', '4500');
+
+
+
+SELECT CUSTOMERS.ID,  NAME, AMOUNT, DATE
+   FROM CUSTOMERS
+   INNER JOIN ORDERS
+   ON CUSTOMERS.ID = ORDERS.Customer_ID;
+   
+   
+SELECT CUSTOMERS.ID,  NAME, AMOUNT, DATE
+   FROM CUSTOMERS
+   left JOIN ORDERS
+   ON CUSTOMERS.ID = ORDERS.Customer_ID;
+   
+SELECT CUSTOMERS.ID,  NAME, AMOUNT, DATE
+   FROM CUSTOMERS
+   right JOIN ORDERS
+   ON CUSTOMERS.ID = ORDERS.Customer_ID;
+   
+   
+   
+create view InnerJoinView as
+SELECT CUSTOMERS.ID,  NAME, AMOUNT, DATE
+   FROM CUSTOMERS
+   INNER JOIN ORDERS
+   ON CUSTOMERS.ID = ORDERS.Customer_ID;
+   
+SELECT name FROM practice.innerjoinview;
+
+SELECT * FROM practice.innerjoinview;
+
+
+#Create INDEX
+
+create index custsalindex on customers(salary);
+
+SELECT * FROM practice.customers  where salary >1000;
